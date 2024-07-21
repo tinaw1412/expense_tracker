f8245a62448ec5535500c0c1076711be3a028e1d
tina <jingnan1412@gmail.com>
2024-07-21 18:03:28 -0400

    debug

--- php/script/init_db.php
+++ php/script/init_db.php
@@ -4,23 +4,19 @@ ini_set('display_errors', '1');
 
 include_once '../DemoConn.php';
 $db=new DemoConn();
-var_dump($db);
 
 $handle = fopen("../sql/0__db_structure", "r");
-var_dump($handle);
 
 if ($handle) {
     $sql="";
+
     while (($line = fgets($handle)) !== false) {
         // process the line read.
-        $sql .= $line;
-        var_dump($line);
-        if (str_ends_with($line, ';')){
-            echo "executing $sql".PHP_EOL;
-            $db->exec($sql);
-            $sql="";
-        }
+        $sql .= $line;  
     }
+    $result=$db->exec($sql);
+    var_dump($result);
+
 
     fclose($handle);
 }
