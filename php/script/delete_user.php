f660d5441f12e43a701f64b59dff656c8a54b1a2
tina <jingnan1412@gmail.com>
2024-09-12 22:18:13 -0400

    script to detele user

---
+++ php/script/delete_user.php
@@ -0,0 +1,14 @@
+<?php
+include_once '../DemoConn.php';
+$db=new DemoConn();
+
+if (count($argv)==2){
+    $email=$argv[1];
+    $sql="delete from users where email=?";
+    $result=$db->exec($sql,[$email]);
+
+    echo "User deleted: $result";
+
+}else{
+    echo "Please enter email to be deleted only";
+}
